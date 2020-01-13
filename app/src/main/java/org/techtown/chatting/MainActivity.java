f2a8ba89db492e7c3950f7d4243f15705a50e2db
lezin <yhjsoar5@gmail.com>
2020-01-13 14:45:27 +0900

    BugFix

--- app/src/main/java/org/techtown/chatting/MainActivity.java
+++ app/src/main/java/org/techtown/chatting/MainActivity.java
@@ -1,6 +1,6 @@
 package org.techtown.chatting;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
